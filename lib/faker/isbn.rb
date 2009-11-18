a58a4321b10894e9b026512384e706c2c7a30538
Overbryd <l.rieder@gmail.com>
2009-11-18 19:51:35 +0100

    Added an isbn 13 faker

--- lib/faker/isbn.rb
+++ lib/faker/isbn.rb
@@ -3,15 +3,30 @@ module Faker
     class << self
       def isbn_10
         isbn_without_checksum = Faker.numerify('#-#####-###-')
-        isbn_without_checksum + checksum(isbn_without_checksum).to_s
+        isbn_without_checksum + checksum_10(isbn_without_checksum).to_s
+      end
+      
+      def isbn_13
+        isbn_without_checksum = Faker.numerify('###-#-####-####-')
+        isbn_without_checksum + checksum_13(isbn_without_checksum).to_s
       end
       
       private
       
-      def checksum(isbn)
-        _isbn = isbn.scan(/\d/).join
+      def checksum_10(isbn)
+        _isbn = digits(isbn)
         sum = (0..8).inject(0) {|sum, i| sum += _isbn[i,1].to_i * (i+1)} % 11
         sum == 10 ? 'X' : sum
+      end
+      
+      def checksum_13(isbn)
+        _isbn = digits(isbn)
+        sum = (0..11).inject(0) { |sum, i| sum += ((i+1) % 2 == 0 ? _isbn[i,1].to_i*3 : _isbn[i,1].to_i) }
+        (10 - sum) % 10
+      end
+      
+      def digits(isbn)
+        isbn.scan(/\d/).join
       end
     end
   end
--- test/test_faker_isbn.rb
+++ test/test_faker_isbn.rb
@@ -6,14 +6,9 @@ class TestFakerIsbn < Test::Unit::TestCase
   end
   
   def test_isbn_10_has_correct_format
-    assert @tester.isbn_10.match(/\d{1}-\d{5}-\d{3}-[\dX]/)
+    assert @tester.isbn_10.match(/^\d{1}-\d{5}-\d{3}-[\dX]$/)
   end
-
-  def test_faker_isbn_10_calculates_correct_checksum
-    Faker.stubs(:numerify).returns '3-86680-192-'
-    assert_equal '3-86680-192-0', @tester.isbn_10
-  end
-
+  
   def test_faker_isbn_10_calculates_correct_checksum
     Faker.stubs(:numerify).returns '3-86680-192-'
     assert_equal '3-86680-192-0', @tester.isbn_10
@@ -22,5 +17,14 @@ class TestFakerIsbn < Test::Unit::TestCase
   def test_faker_isbn_replacs_a_checksm_of_10_with_an_x
     Faker.stubs(:numerify).returns '8-90476-338-'
     assert_equal "8-90476-338-X", @tester.isbn_10
+  end
+  
+  def test_isbn_13_has_correct_format
+    assert @tester.isbn_13.match(/^\d{3}-\d{1}-\d{4}-\d{4}-\d$/)
+  end
+
+  def test_faker_isbn_13_calculates_correct_checksum
+    Faker.stubs(:numerify).returns '978-3-7657-2781-'
+    assert_equal '978-3-7657-2781-8', @tester.isbn_13
   end
 end
